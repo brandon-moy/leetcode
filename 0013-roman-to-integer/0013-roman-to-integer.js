fce5f37f6ce6b6147ea41a2995f80e3f664a0ab1
Brandon Moy <brandon@brandonmoy.com>
2023-03-09 18:28:01 -0800

    Time: 145 ms (28.09%), Space: 48.7 MB (17.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let output = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'I') {
+            if (s[i + 1] === 'V' ) {
+                output += 4;
+                i++;
+            } else if (s[i + 1] === 'X') {
+                output += 9;
+                i++;
+            } else {
+                output += 1;
+            }
+        } else if (s[i] === 'V') {
+            output += 5;
+        } else if (s[i] === 'X') {
+            if (s[i + 1] === 'L' ) {
+                output += 40;
+                i++;
+            } else if (s[i + 1] === 'C') {
+                output += 90;
+                i++;
+            } else {
+                output += 10;
+            }
+        } else if (s[i] === 'L') {
+            output += 50;
+        } else if (s[i] === 'C') {
+            if (s[i + 1] === 'D' ) {
+                output += 400;
+                i++;
+            } else if (s[i + 1] === 'M') {
+                output += 900;
+                i++;
+            } else {
+                output += 100;
+            }
+        } else if (s[i] === 'D') {
+            output += 500;
+        } else if (s[i] === 'M') {
+            output += 1000;
+        }
+    }
+    return output;
+};
