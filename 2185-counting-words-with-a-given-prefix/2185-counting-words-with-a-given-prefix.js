6b4de7b51829f460853162c5a8bf4de9a5bde7ad
Brandon Moy <brandon@brandonmoy.com>
2024-01-25 16:31:45 -0800

    Time: 55 ms (45.97%), Space: 49.7 MB (5.01%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+    let output = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].startsWith(pref)) {
+            output++
+        }
+    }
+    return output;
+};
