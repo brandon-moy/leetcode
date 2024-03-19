1df05b7043b2d22766cca34d246933a5c2d0b705
Brandon Moy <brandonkaghen@gmail.com>
2024-03-18 18:06:31 -0700

    Time: 92 ms (41.08%), Space: 54 MB (45.25%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let charMap = [];
+    for (let i = 0; i < s.length; i++) {
+        if(charMap[s[i]]) {
+            charMap[s[i]] ++;
+        } else {
+            charMap[s[i]] = 1;
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (charMap[s[i]] === 1) {
+            return i;
+        }
+    }
+    return -1;
+};
